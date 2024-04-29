25090699f9e6e25efcfd85868cd31abeffff2547
codesantaisai <143990097+Jathurshanliwe@users.noreply.github.com>
2024-04-29 15:10:09 +0530

    setup finished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Header from './layout/Header';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header/>
   </React.StrictMode>
 );
 
